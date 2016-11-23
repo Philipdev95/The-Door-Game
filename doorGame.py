242e8e68788245d8e8359417cff86f4025503e58
Philipdev95 <philipdev@icloud.com>
2016-11-23 17:43:50 +0100

    doorGame file

---
+++ doorGame.py
@@ -0,0 +1,10 @@
+#-*- coding: utf-8 -*-
+
+class Doors:
+    def __init__(self, doorNr, doorHp, playerHp):
+        self.doorNr = doorNr
+        self.doorHp = doorHp
+        self.playerHp = playerHp
+        
+    def attack_door(self, doorNr):
+        pass
