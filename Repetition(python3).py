57a73e0a1213e811ca2b65a92c1dcc33d4e9d713
Philipdev95 <philipdev@icloud.com>
2016-11-23 17:21:55 +0100

    START

---
+++ Repetition(python3).py
@@ -0,0 +1,7 @@
+#Nytt i python3
+#funktionen "print()"
+#division med heltal
+#str är unicode som standard (underlättar åäö)
+#"input()" returnerar en sträng ("raw_input()" finns inte längre)
+#funktionen "round()" avrundar vid närmsta jämna siffra
+#   import future
