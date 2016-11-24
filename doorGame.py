563ec7bf6f739236517256b44022936c97afbf19
Philipdev95 <philipdev@icloud.com>
2016-11-24 23:40:31 +0100

    some of dat code

--- doorGame.py
+++ doorGame.py
@@ -1,5 +1,5 @@
 #-*- coding: utf-8 -*-
-
+import time
 class Doors:
     def __init__(self, doorNr, doorHp, playerHp):
         self.doorNr = doorNr
@@ -7,4 +7,34 @@ class Doors:
         self.playerHp = playerHp
         
     def attack_door(self, doorNr):
+        '''Om man som anvndare ska kunna attackera dörrar för att kunna öppna dem?'''
         pass
+
+    
+def one():
+    print (35*"-")
+    print (10*" " + " W E L C O M E ")
+    time.sleep(0.4)
+    print (10*" " + "    TO THE    ")
+    time.sleep(0.4)
+    print (10*" " + "  DOOR  GAME  ")
+    time.sleep(0.4)
+    print (35*"-")
+    
+    
+    
+def menu():
+    print ("1. Starta Spelet")
+    print ("2. Statistik")
+    print ("3. Avsluta")
+    menuval()
+def menuval():
+    mv = input("Ditt val: ")
+    if mv is 1:
+        one()
+    elif mv is 2:
+        two()
+    elif mv is 3:
+        exit()
+    
+menu()
