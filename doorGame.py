2b2c3934913ff1400425016c8a48227a5bd80c71
Philipdev95 <philipdev@icloud.com>
2016-11-25 21:41:42 +0100

    changes

--- doorGame.py
+++ doorGame.py
@@ -1,5 +1,9 @@
 #-*- coding: utf-8 -*-
 import time
+import random
+
+
+'''
 class Doors:
     def __init__(self, doorNr, doorHp, playerHp):
         self.doorNr = doorNr
@@ -7,9 +11,34 @@ class Doors:
         self.playerHp = playerHp
         
     def attack_door(self, doorNr):
-        '''Om man som anvndare ska kunna attackera dörrar för att kunna öppna dem?'''
+        Om man som anvndare ska kunna attackera dörrar för att kunna öppna dem?
         pass
-
+'''
+    
+def chooseDoor():
+    doorlist = [
+        'Första', 'Andra', 'Tredje'
+    ]
+    print ("Its time to choose your first door!")
+    print ("Det finns 3 dörrar framför dig.")
+    time.sleep(0.6)
+    print ("Välj mellan 1, 2 eller 3. Eller skriv random för att låta oss välja!")
+    dc = input ("Ditt val: ")
+    if dc is 1:
+        print ("Du har valt dörr nummer 1")
+        
+    elif dc is 2:
+        print ("Du har valt dörr nummer 2")
+        
+    elif dc is 3:
+        print ("Du har valt dörr nummer 3")
+        
+    else: 
+        print ("...")
+        time.sleep(0.5)
+        '''kör om frågan'''
+    
+    #print (random.choice(doorlist))
     
 def one():
     print (35*"-")
@@ -20,7 +49,8 @@ def one():
     print (10*" " + "  DOOR  GAME  ")
     time.sleep(0.4)
     print (35*"-")
-    
+    time.sleep(1.5)
+    chooseDoor()
     
     
 def menu():
